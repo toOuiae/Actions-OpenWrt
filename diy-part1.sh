8ce50a69e5644d5cc86eb9526bc6a91a4e5851b0
Ouiae <76885266@qq.com>
2020-05-18 17:51:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git rosy https://github.com/toOuiae/luci-theme-rosy.git' feeds.conf.default
+sed -i '$a src-git clash https://github.com/frainzy1477/clash.git' feeds.conf.default
+#sed -i '$a src-git rosy https://github.com/toOuiae/luci-theme-rosy.git' feeds.conf.default
